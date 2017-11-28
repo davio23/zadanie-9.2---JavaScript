482d940b4a973a4730eb397b631c8ac2eb85c600
davio23 <33763332+davio23@users.noreply.github.com>
2017-11-28 23:11:45 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,4 +1,9 @@
-var a=6;
-var h=7; 
-var triangleArea=a*h/2; 
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames= femaleNames.concat(maleNames);
+var newName = ('Marian');
+if (allNames.indexOf(newName) === -1) ;{
+console.log('yes');
+}
+var x= allNames.push('Marian');
+console.log(allNames);
